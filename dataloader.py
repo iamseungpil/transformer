97cb893364aa7abcb0df649cc5d42d8240983df9
Seungpil <iamseungpil@gm.gist.ac.kr>
2023-11-25 18:36:26 +0900

    batch size reduced

--- dataloader.py
+++ dataloader.py
@@ -4,7 +4,82 @@ import glob
 import io
 import codecs
 
-from torchtext.datasets import TranslationDataset
+from torchtext import data
+
+class TranslationDataset(data.Dataset):
+    """Defines a dataset for machine translation."""
+
+    @staticmethod
+    def sort_key(ex):
+        return data.interleave_keys(len(ex.src), len(ex.trg))
+
+    def __init__(self, path, exts, fields, **kwargs):
+        """Create a TranslationDataset given paths and fields.
+
+        Arguments:
+            path: Common prefix of paths to the data files for both languages.
+            exts: A tuple containing the extension to path for each language.
+            fields: A tuple containing the fields that will be used for data
+                in each language.
+            Remaining keyword arguments: Passed to the constructor of
+                data.Dataset.
+        """
+        if not isinstance(fields[0], (tuple, list)):
+            fields = [('src', fields[0]), ('trg', fields[1])]
+
+        src_path, trg_path = tuple(os.path.expanduser(path + x) for x in exts)
+
+        examples = []
+       
+        with io.open(src_path, mode='r', encoding='utf-8') as src_file, \
+                io.open(trg_path, mode='r', encoding='utf-8') as trg_file:
+            
+            src_lines = src_file.read().split('\n')
+            trg_lines = trg_file.read().split('\n')
+
+            c = list(zip(src_lines, trg_lines))
+            examples = [data.Example.fromlist([x, y], fields) for x, y in c]
+            # examples.append(data.Example.fromlist([src_line, trg_line], fields))
+
+
+            # for src_line, trg_line in zip(src_file, trg_file):
+            #     src_line, trg_line = src_line.strip(), trg_line.strip()
+            #     if src_line != '' and trg_line != '':
+                #   examples.append(data.Example.fromlist(
+                #     [src_line, trg_line], fields))
+
+
+        super(TranslationDataset, self).__init__(examples, fields, **kwargs)
+
+    @classmethod
+    def splits(cls, exts, fields, path=None, root='.data',
+               train='train', validation='val', test='test', **kwargs):
+        """Create dataset objects for splits of a TranslationDataset.
+
+        Arguments:
+            exts: A tuple containing the extension to path for each language.
+            fields: A tuple containing the fields that will be used for data
+                in each language.
+            path (str): Common prefix of the splits' file paths, or None to use
+                the result of cls.download(root).
+            root: Root dataset storage directory. Default is '.data'.
+            train: The prefix of the train data. Default: 'train'.
+            validation: The prefix of the validation data. Default: 'val'.
+            test: The prefix of the test data. Default: 'test'.
+            Remaining keyword arguments: Passed to the splits method of
+                Dataset.
+        """
+        if path is None:
+            path = cls.download(root)
+
+        train_data = None if train is None else cls(
+            os.path.join(path, train), exts, fields, **kwargs)
+        val_data = None if validation is None else cls(
+            os.path.join(path, validation), exts, fields, **kwargs)
+        test_data = None if test is None else cls(
+            os.path.join(path, test), exts, fields, **kwargs)
+        return tuple(d for d in (train_data, val_data, test_data)
+                     if d is not None)
 
 class Multi30k(TranslationDataset):
     """The small-dataset WMT 2016 multimodal task, also known as Flickr30k"""
@@ -18,7 +93,7 @@ class Multi30k(TranslationDataset):
 
     @classmethod
     def splits(cls, exts, fields, root='.data',
-               train='train', validation='val', test='test', **kwargs):
+            train='train', validation='val', test='test', **kwargs):
         """Create dataset objects for splits of the Multi30k dataset.
 
         Arguments:
@@ -59,9 +134,9 @@ class WMT14(TranslationDataset):
 
     @classmethod
     def splits(cls, exts, fields, root='.data',
-               train='train',
-               validation='newstest2013',
-               test='newstest2014', **kwargs):
+            train='train',
+            validation='newstest2013',
+            test='newstest2014', **kwargs):
         """Create dataset objects for splits of the WMT 2014 dataset.
 
         Arguments:
